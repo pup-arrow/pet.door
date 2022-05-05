d66c67e415f023710cb0e40c4b9c0c032e33bd9d
pup-arrow <n254@sbcglobal.net>
2022-05-04 22:25:20 -0700

    interrupts
    added interrupts for open button and signal from app. Open and closed state roughly coded

--- pet.door/pet_door/pet_door.ino
+++ pet.door/pet_door/pet_door.ino
@@ -1,9 +1,9 @@
-//Portion of code to operate motor used from Handson Technology
-
 // Definitions Arduino pins connected to input H Bridge 
 int IN1 = 4; 
 int IN2 = 5; 
-const unit8_t OPEN_BTN = ?; //OPEN_BTN connected to ?
+const unit8_t OPEN_BTN = 2; //OPEN_BTN connected to pin 2 external interrupt
+const unit8_t OPEN_APP = 3; //OPEN_APP connected to pin 3 external interrupt
+const unit8_t PWR_LED = ?;  //PWR_LED connected to ?
 
 enum states {
   IDLE,
@@ -19,36 +19,84 @@ void setup() {
   // Set the output pins
   pinMode(IN1, OUTPUT); 
   pinMode(IN2, OUTPUT); 
-
+  digitalWrite(PWR_LED, HIGH);
+  attachInterrupt(digitalPinToInterrupt(2),openbtnPressed,RISING); 
+  attachInterrupt(digitalPinToInterrupt(3),openappPressed,RISING); 
 }
 
 void loop() {
+  void openbtnPressed(){ //open button pressed interrupt        
+   if (state != OPEN){ // if already in open state do nothing else stop motor
+    digitalWrite(IN1, HIGH); 
+    digitalWrite(IN2, HIGH);
+    state = OPEN;
+   }
+  }
+  void openappPressed(){ //open signal from app interrupt         
+   if (state != OPEN){ // if already in open state do nothing else stop motor
+    digitalWrite(IN1, HIGH); 
+    digitalWrite(IN2, HIGH);
+    state = OPEN;
+   }
+  }
   switch(state){
     case IDLE:
-    if (digitalRead(OPEN_BTN) == HIGH){ // if open button is pushed on door 
-      state = OPEN;  // switch to open state
+    if (state == OPEN){
+      break;  // switch to open state
     }
     break;
 
     case OPEN:
-    // Rotates the Motor counter-clockwise digitalWrite(IN1, LOW); 
+    // Rotates the Motor counter-clockwise 
+    digitalWrite(IN1, LOW);  //turns on motor  need to figure out pwm to contrl speed
     digitalWrite(IN2, HIGH); 
-    delay(2000); 
-    digitalWrite(IN1, HIGH); 
+    delay(1000); //time motor runs (1s)  how to set a top limit to where dooe is raised every time?
+    digitalWrite(IN1, HIGH); //stops motor
     digitalWrite(IN2, HIGH); 
-    delay(500); 
+    SEEN:
+    delay(4000); //time before door will check to close (4s)
+    if (reader == tag){//reader still see tag?
+      goto SEEN; //return to delay and check for tag again
+    state = CLOSE
     break;
 
     case CLOSE:
-    if (digitalRead(OPEN_BTN) == HIGH){ // if open button is pushed on door 
-      state = OPEN;  // switch to open state
-    }
-    // Rotate the Motor clockwise digitalWrite(IN1, HIGH); 
-    digitalWrite(IN2, LOW); 
-    delay(2000); 
+    // Rotate the Motor clockwise 
     digitalWrite(IN1, HIGH); 
-    digitalWrite(IN2, HIGH); 
-    delay(500); 
+    digitalWrite(IN2, LOW); //turns on motor  need to figure ut pwm to contrl speed
+    if (state == OPEN){break;} //if open btn or signal leave close state
+    delay(1000); // 1s
+    if (SW1 != HIGH || state == OPEN){ //obstruction in door 
+      digitalWrite(IN1, HIGH); 
+      digitalWrite(IN2, HIGH);
+      state = OPEN;
+      break;
+    }
+    delay(1000); 
+    if (SW2 != HIGH || state == OPEN){ //obstruction in door 
+      digitalWrite(IN1, HIGH); 
+      digitalWrite(IN2, HIGH);
+      state = OPEN;
+      break;
+    }
+    delay(1000); 
+    if (SW3 != HIGH || state == OPEN){ //obstruction in door 
+      digitalWrite(IN1, HIGH); 
+      digitalWrite(IN2, HIGH);
+      state = OPEN;
+      break;
+    }
+    delay(1000); 
+    if (SW4 != HIGH || state == OPEN){ //obstruction in door 
+      digitalWrite(IN1, HIGH); 
+      digitalWrite(IN2, HIGH);
+      state = OPEN;
+      break;
+    }
+    delay(1000); //time motor runs (1s)
+    digitalWrite(IN1, HIGH); 
+    digitalWrite(IN2, HIGH); //stops motor
+    state = IDLE;
     break;
   }
 
