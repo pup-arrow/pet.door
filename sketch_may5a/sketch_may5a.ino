3be124d2a00281fe64c8b072692afb3243a8fd12
pup-arrow <n254@sbcglobal.net>
2022-05-31 00:56:55 -0700

    Update sketch_may5a.ino
    fiddling with code to test motor

--- sketch_may5a/sketch_may5a.ino
+++ sketch_may5a/sketch_may5a.ino
@@ -1,10 +1,10 @@
-// Definitions Arduino pins connected to input H Bridge 
+// Definitions Arduino pins connected to input H Bridge
 // #define ON 1
 // #define OFF 0
 #define MAX 255
 #define MIN 0
-int IN1 = 0; //motor pwm pin values 0-255
-int IN2 = 4; //motor pwm pin vlues 0-255
+int IN1 = 0;  //motor pwm pin values 0-255
+int IN2 = 4;  //motor pwm pin vlues 0-255
 
 //PWM setup
 // const int freq = 5000;
@@ -16,71 +16,73 @@ int IN2 = 4; //motor pwm pin vlues 0-255
 void setup() {
   // Set the output pins
   Serial.begin(115200);
-  // Serial.println("test");
-  // pinMode(IN1, OUTPUT); 
-  // pinMode(IN2, OUTPUT); 
-//  ledcSetup(ledChannel, freq, resolution);
-//  ledcSetup(ledChannel4, freq, resolution);
-//  ledcAttachPin(IN1, ledChannel);
-//  ledcAttachPin(IN2, ledChannel);
-//  ledcWrite(ledChannel, MAX);
-//  ledcWrite(ledChannel4, MAX);
+  Serial.println("test");
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  //  ledcSetup(ledChannel, freq, resolution);
+  //  ledcSetup(ledChannel4, freq, resolution);
+  //  ledcAttachPin(IN1, ledChannel);
+  //  ledcAttachPin(IN2, ledChannel);
+  //  ledcWrite(ledChannel, MAX);
+  //  ledcWrite(ledChannel4, MAX);
   // analogWrite(IN1, MAX); //stops motor
   // analogWrite(IN2, MAX);
-  
-  digitalWrite(IN1, MAX); //stops motor
-  digitalWrite(IN2, MAX);
-  // Serial.println("after test");
-} 
+
+  // digitalWrite(IN1, HIGH); //stops motor
+  // digitalWrite(IN2, HIGH);
+  Serial.println("after test");
+}
 
 void loop() {
   // Serial.println("start loop");
   // Rotates the Motor counter-clockwise
-  // delay(2000); 
-  // analogWrite(IN1, MAX); 
+  // delay(2000);
+  // analogWrite(IN1, MAX);
   // analogWrite(IN2, MAX); //stops motor
   // ledcWrite(ledChannel, MAX);
   // ledcWrite(ledChannel4, MAX);
-  digitalWrite(IN1, MAX); //stops motor
-  digitalWrite(IN2, MAX);
-  delay(3000);
+  digitalWrite(IN1, HIGH);  //stops motor
+  digitalWrite(IN2, HIGH);
+  Serial.println("stop!");
+  delay(1000);
 
   // analogWrite(IN1, MAX);  //turns on motor  (need to figure out pwm to contrl speed)
-  // analogWrite(IN2, MIN); 
+  // analogWrite(IN2, MIN);
   // ledcWrite(ledChannel, MAX);
   // ledcWrite(ledChannel4, MIN);
-  digitalWrite(IN1, MAX); //stops motor
-  digitalWrite(IN2, MIN);
-  // Serial.println("motor ccw!");
+  digitalWrite(IN1, HIGH);  //stops motor
+  digitalWrite(IN2, LOW);
+  Serial.println("motor ccw!");
 
-  delay(3000);
+  delay(8000);
   // analogWrite(IN1, MAX); //stops motor
-  // analogWrite(IN2, MAX); 
+  // analogWrite(IN2, MAX);
   // Serial.println("off");
 
   // delay(2000);
 
-  // Rotate the Motor clockwise 
-  // analogWrite(IN1, MAX); 
+  // Rotate the Motor clockwise
+  // analogWrite(IN1, MAX);
   // analogWrite(IN2, 192); //turns on motor  need to figure out pwm to contrl speed
   // Serial.println("motor cw");
   // ledcWrite(ledChannel, MAX);
   // ledcWrite(ledChannel4, MAX);
-  // delay(5000); 
-  digitalWrite(IN1, MAX); //stops motor
-  digitalWrite(IN2, MAX);
-  // analogWrite(IN1, MAX); 
+  // delay(5000);
+  digitalWrite(IN1, HIGH);  //stops motor
+  digitalWrite(IN2, HIGH);
+  Serial.println("stop!");
+  // analogWrite(IN1, MAX);
   // analogWrite(IN2, MAX); //stops motor
-  
-  delay(3000);
 
-  // analogWrite(IN1, MIN); 
+  delay(1000);
+
+  // analogWrite(IN1, MIN);
   // analogWrite(IN2, MAX); //stops motor
   // ledcWrite(ledChannel4, MAX);
   // ledcWrite(ledChannel, MIN);
-  digitalWrite(IN1, MIN); //stops motor
-  digitalWrite(IN2, MAX);
+  digitalWrite(IN1, LOW);  //stops motor
+  digitalWrite(IN2, HIGH);
+  Serial.println("motor CW!");
   // Serial.println("off");
-  delay(3000);
-
+  delay(4000);
 }
