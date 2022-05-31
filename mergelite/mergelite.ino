0717e182ddb307a3ccb13ab8ebc8c29439f6e45f
pup-arrow <n254@sbcglobal.net>
2022-05-31 03:01:19 -0700

    Update mergelite.ino
    Testing and debugging code on door

--- mergelite/mergelite.ino
+++ mergelite/mergelite.ino
@@ -5,10 +5,10 @@
 #include <Preferences.h>
 // #define Infrare1Pin 25
 // #define Infrare2Pin 5
-#define RFCHPin 27
-#define REDLED 15
-#define WHITELED 14
-#define GREENLED 12
+// #define RFCHPin 27
+// #define REDLED 15
+// #define WHITELED 14
+// #define GREENLED 12
 
 // #define IN4 0
 // #define IN3 2
@@ -22,27 +22,28 @@
 #define SCK_PIN   18 
 
 // Definitions Arduino pins connected to input H Bridge 
-#define IN1 0 //motor pwm pin values 0-255 (D9)
+#define IN1 27 //motor pwm pin values 0-255 (D11)
 #define IN2 4 //motor pwm pin vlues 0-255 (D10)
 #define SW1 26 //top switch (D12)
-#define SW2 27 //second to top switch (D11)
-#define SW3 2 // second to bottom switch (D8)
-#define SW4 5 // bottom switch (D7)
+#define SW2 2 //second to top switch (D8) 
+#define SW3 5 // second to bottom switch (D7)
+#define SW4 15 // bottom switch (D5)
 #define OPEN_BTN 13 //OPEN_BTN connected to pin 2 external interrupt (D2)
-#define OPEN_APP 3 //OPEN_APP connected to pin 3 external interrupt
+// #define RFID_PWR 12 //OPEN_APP connected to pin 3 external interrupt (D3)
 #define PWR_LED 25  //PWR_LED connected to (D6)
-#define FMD 15 //front motion detechtor (D5)
+#define FMD 36 //front motion detechtor (S0)
 #define MAX 255
 #define MIN 0
 
-int RFID_PWR = HIGH; 
+// int RFID_PWR = HIGH; 
 int old_FMD;  
 int cur_FMD = HIGH;
 
 // need to research how to permanently store tag #s and read RFID #s
 
-int reader = LOW;
-int tag = LOW;
+// int reader = LOW;
+// int tag = LOW;
+int stall = 0;
 
 enum states {
   IDLE,
@@ -57,9 +58,12 @@ Preferences preferences;
 //
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance
-const char* ssid = "Yuriy";
-const char* password = "Cy09080701";
+// Hardcoded credentials for WiFi HotSpot
+//mosquitto linux
+const char* ssid = "Arrow";
+const char* password = "5805747059";
 const char* mqtt_server = "124.71.19.61";
+
 
 
 WiFiClient espClient;
@@ -74,13 +78,14 @@ String DeviceName[10]={"RFID","DoorStatus","Infrare1","Infrare2","Alarm"};
 String DeviceStatus[10]={"0","0","0","0","0"};
 String RFIDList[100];
 String ReCntrl[10]={"OpenDoor","TimerStatus","RFIDADD","RFIDDELE"};
+String StrRFID;
 int ReOpenDoor=0;
 int ReTimerStatus=1;
 int RFIDCount=0;
 const char *RECERFID = NULL;  
-bool door_open_flag; //开门动作标志位
-void setup_wifi() {
+bool door_open_flag; 
 
+void setup_wifi() { //Bluetooth to WiFi code too large to hardcoding Wifi setup
   delay(10);
   // We start by connecting to a WiFi network
   Serial.println();
@@ -103,58 +108,49 @@ void setup_wifi() {
   Serial.println(WiFi.localIP());
 }
 
-void parmPass(JsonVariant parm) //Json means MQTT parameter
-{
-    if (parm.containsKey(ReCntrl[0])) //ReCntrl[0] = app open button
-    {
-        ReOpenDoor=parm[ReCntrl[0]];
-        if(ReOpenDoor)
-        {
-            digitalWrite(OPEN_APP, HIGH);
-          }
-      snprintf (msg, MSG_BUFFER_SIZE, "{\"DoorStatus\":\"1\"}"); //DoorStatus "1" means door is open
-      Serial.print("Publish message: ");
-      Serial.println(msg);
-      client.publish("outTopic", msg);
+void parmPass(JsonVariant parm){ //Json means MQTT parameter
+  if (parm.containsKey(ReCntrl[0])){ //ReCntrl[0] = app open button
+    ReOpenDoor=parm[ReCntrl[0]];
+    if(ReOpenDoor){
+      state = OPEN;
     }
-    else  if (parm.containsKey(ReCntrl[1]))
-    {
-      ReTimerStatus=parm[ReCntrl[1]];
-      Serial.println("ReTimerStatus");
-      Serial.println(ReTimerStatus);
-      }
-    else  if (parm.containsKey(ReCntrl[2])) // Open if pet ID match RFID
-    {
-       RECERFID=parm[ReCntrl[2]];
-       RFIDList[RFIDCount++]=RECERFID;
-       
-      Serial.println("RFIDList:");
-      for(int i=0;i<RFIDCount;i++)
+    snprintf (msg, MSG_BUFFER_SIZE, "{\"DoorStatus\":\"1\"}"); //DoorStatus "1" means door is open
+    Serial.print("Publish message: ");
+    Serial.println(msg);
+    client.publish("outTopic", msg);
+  }
+  else  if (parm.containsKey(ReCntrl[1])){
+    ReTimerStatus=parm[ReCntrl[1]];
+    Serial.println("ReTimerStatus");
+    Serial.println(ReTimerStatus);
+  }
+  else  if (parm.containsKey(ReCntrl[2])){ // Open if pet ID match RFID
+    RECERFID=parm[ReCntrl[2]];
+    RFIDList[RFIDCount++]=RECERFID;
+    Serial.println("RFIDList:");
+    for(int i=0;i<RFIDCount;i++){
       Serial.println(RFIDList[i]);
-      }
-    else  if (parm.containsKey(ReCntrl[3])) //Pet ID
-    {
-       RECERFID=parm[ReCntrl[3]];
-       RFIDCount--;
-      for(int i=0;i<100;i++)
-      {
-        if(RFIDList[i]==RECERFID)
-        {
-          if(i==99)
-            RFIDList[i]="-1";
-          for(int j=i;j<100-1;j++)
-            RFIDList[j]=RFIDList[j+1];
+    }
+  }
+  else  if (parm.containsKey(ReCntrl[3])){ //Pet ID
+    RECERFID=parm[ReCntrl[3]];
+    RFIDCount--;
+    for(int i=0;i<100;i++){
+      if(RFIDList[i]==RECERFID){
+        if(i==99){
+          RFIDList[i]="-1";
         }
+        for(int j=i;j<100-1;j++){
+          RFIDList[j]=RFIDList[j+1];
         }
-      Serial.println("RFIDList:");
-      for(int i=0;i<RFIDCount;i++)
-      Serial.println(RFIDList[i]);
       }
-
-   
-
-   
+    }
+    Serial.println("RFIDList:");
+    for(int i=0;i<RFIDCount;i++)
+    Serial.println(RFIDList[i]);
+  }   
 }
+
 void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Message arrived [");
   Serial.print(topic);
@@ -187,7 +183,7 @@ void reconnect() {
     // Attempt to connect
     if (client.connect(clientId.c_str(),"bysj","123")) {
       Serial.println("connected");
-//      // Once connected, publish an announcement...
+    // Once connected, publish an announcement...
       client.subscribe("inTopic");
     } else {
       Serial.print("failed, rc=");
@@ -198,7 +194,7 @@ void reconnect() {
     }
   }
 }
-String StrRFID;
+
 void readRFID()
 {
   // Prepare key - all keys are set to FFFFFFFFFFFFh at chip delivery from the factory.
@@ -310,110 +306,111 @@ void readRFID()
   mfrc522.PICC_HaltA();
   mfrc522.PCD_StopCrypto1();
   }
+
+
 void setup() {
   state = IDLE;  //initate in the idle state
-  pinMode(REDLED,OUTPUT);//RED 警报灯
-  pinMode(WHITELED,OUTPUT);//WHITE 门口灯
-  pinMode(GREENLED,OUTPUT);//GREEN 门内灯
+  // pinMode(REDLED,OUTPUT);//RED Error?
+  // pinMode(WHITELED,OUTPUT);//WHITE Motion sensor?
+  // pinMode(GREENLED,OUTPUT);//GREEN RFID read?
   pinMode(FMD, INPUT);
-  pinMode(RFCHPin,INPUT);//RFID切换
+  // pinMode(RFCHPin,INPUT);//RFID
   pinMode(IN1, OUTPUT); 
   pinMode(IN2, OUTPUT); 
   pinMode(OPEN_BTN, INPUT);
   pinMode(PWR_LED, OUTPUT);
   digitalWrite(PWR_LED, HIGH);
-  analogWrite(IN1, MAX); //stops motor
-  analogWrite(IN2, MAX); 
+  analogWrite(IN1, MIN); //stops not energized
+  analogWrite(IN2, MIN); 
   attachInterrupt(digitalPinToInterrupt(OPEN_BTN),openbtnPressed,RISING); 
-  attachInterrupt(digitalPinToInterrupt(3),openappPressed,RISING); 
+  // attachInterrupt(digitalPinToInterrupt(RFID_PWR),RFIDbtnPressed,RISING); 
   preferences.begin("pet_id", false); // allows for saving id #s to permanent memory
   Serial.begin(115200);
-  SPI.begin(SCK_PIN, MISO_PIN, MOSI_PIN);                                                 // Init SPI bus
-  mfrc522.PCD_Init();                                              // Init MFRC522 card
+  SPI.begin(SCK_PIN, MISO_PIN, MOSI_PIN); // Init SPI bus
+  mfrc522.PCD_Init();                    // Init MFRC522 card
   Serial.println(F("Read personal data on a MIFARE PICC:"));    //shows in serial that it is ready to read
   setup_wifi();
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
- 
+  state = OPEN;
 }
-int a=0;
+
 void loop() {
   switch(state){
     case IDLE:
     if (state == OPEN){
       break;  // switch to open state
     }
-    if (a==1){
+    if (stall==1){
      client.publish("outTopic","{\"SystemStart\":\"1\"}");
     }
-    else if(a==0){
+    else if(stall==0){
      delay(5000);
-     a++;
+     stall++;
     }
     if (!client.connected()) {
       reconnect();
     }
     client.loop();
-    if(ReTimerStatus==1)
-    {
-    // int Infrare1num=digitalRead(Infrare1Pin);
-    // int Infrare2num=digitalRead(Infrare2Pin);
-    int RFIDCHnum=digitalRead(RFCHPin);
-    if(RFIDCHnum==1)
-    {
-      snprintf (msg, MSG_BUFFER_SIZE, "{\"DoorStatus\":\"0\"}"); // 0 means door closed
-      Serial.print("Publish message: ");
-      Serial.println(msg);
-      client.publish("outTopic", msg);
-    }
+  //   if(ReTimerStatus==1)
+  //   {
+  //   // int Infrare1num=digitalRead(Infrare1Pin);
+  //   // int Infrare2num=digitalRead(Infrare2Pin);
+  //   int RFIDCHnum=digitalRead(RFCHPin);
+  //   if(RFIDCHnum==1)
+  //   {
+  //     snprintf (msg, MSG_BUFFER_SIZE, "{\"DoorStatus\":\"0\"}"); // 0 means door closed
+  //     Serial.print("Publish message: ");
+  //     Serial.println(msg);
+  //     client.publish("outTopic", msg);
+  //   }
 
-    StrRFID="11111111111111111111111111111111";
-    if(Infrare2num)
-    {
-      digitalWrite(GREENLED,HIGH);
-      delay(1500);
-      digitalWrite(GREENLED,LOW);
-    }
-  if(Infrare1num)
-    {
-      digitalWrite(WHITELED,HIGH);
-      delay(1500);
-      digitalWrite(WHITELED,LOW);
-      readRFID();
-      int RFflag=0;
-      for(int i=0;i<100;i++)
-        if(RFIDList[i]==StrRFID)
-          RFflag=1;
-      if(RFflag)
-      {
-        String StrRFIDtemp="{\"RFID\":\""+StrRFID+"\",\"DoorS\":\"1\"}";
-        snprintf (msg, MSG_BUFFER_SIZE,StrRFIDtemp.c_str());
-        Serial.print("Publish message: ");
-        Serial.println(msg);
-        client.publish("outTopic", msg);
-        state = OPEN;
-        break;
-      }
-      else
-      {
-        if(StrRFID!="11111111111111111111111111111111")
-        {
-          Serial.println(RFIDList[0]);
-          snprintf (msg, MSG_BUFFER_SIZE, "{\"Alarm\":\"1\"}");
-          Serial.print("Publish message: ");
-          Serial.println(msg);
-          client.publish("outTopic", msg);
-          digitalWrite(REDLED,HIGH);
-          delay(1500);
-          digitalWrite(REDLED,LOW);
-        } 
-      }
-    }
+  //   StrRFID="11111111111111111111111111111111";
+  //   if(Infrare2num)
+  //   {
+  //     digitalWrite(GREENLED,HIGH);
+  //     delay(1500);
+  //     digitalWrite(GREENLED,LOW);
+  //   }
+  // if(Infrare1num)
+  //   {
+  //     digitalWrite(WHITELED,HIGH);
+  //     delay(1500);
+  //     digitalWrite(WHITELED,LOW);
+  //     readRFID();
+  //     int RFflag=0;
+  //     for(int i=0;i<100;i++)
+  //       if(RFIDList[i]==StrRFID)
+  //         RFflag=1;
+  //     if(RFflag)
+  //     {
+  //       String StrRFIDtemp="{\"RFID\":\""+StrRFID+"\",\"DoorS\":\"1\"}";
+  //       snprintf (msg, MSG_BUFFER_SIZE,StrRFIDtemp.c_str());
+  //       Serial.print("Publish message: ");
+  //       Serial.println(msg);
+  //       client.publish("outTopic", msg);
+  //       state = OPEN;
+  //       break;
+  //     }
+  //     else
+  //     {
+  //       if(StrRFID!="11111111111111111111111111111111")
+  //       {
+  //         Serial.println(RFIDList[0]);
+  //         snprintf (msg, MSG_BUFFER_SIZE, "{\"Alarm\":\"1\"}");
+  //         Serial.print("Publish message: ");
+  //         Serial.println(msg);
+  //         client.publish("outTopic", msg);
+  //         digitalWrite(REDLED,HIGH);
+  //         delay(1500);
+  //         digitalWrite(REDLED,LOW);
+  //       } 
+  //     }
+  //   }
     old_FMD = cur_FMD;
     cur_FMD = digitalRead(FMD);
     if (old_FMD == LOW && cur_FMD == HIGH){
       //send notification to app for inside movement
-
     }
     break;
   case OPEN:
@@ -427,18 +424,18 @@ void loop() {
     delay(1000);
     analogWrite(IN1, MAX); //stops motor
     analogWrite(IN2, MAX); 
-    SEEN:
+    // SEEN:
     delay(4000); //time before door will check to close (4s)
-    if (reader == tag){//reader still see tag?
-      goto SEEN; //return to delay and check for tag again
-    }
+    // if (reader == tag){//reader still see tag?
+    //   goto SEEN; //return to delay and check for tag again
+    // }
     state = CLOSE;
     break;
 
     case CLOSE:
     // Rotate the Motor clockwise 
-    analogWrite(IN1, MAX); 
-    analogWrite(IN2, 192); //turns on motor  need to figure out pwm to contrl speed
+    analogWrite(IN1, MIN); 
+    analogWrite(IN2, MIN); //turns on motor  need to figure out pwm to contrl speed
     if (state == OPEN){break;} //if open btn or signal leave close state
     delay(1000); // 1s
     if (SW1 != LOW || state == OPEN){ //obstruction in door 
@@ -485,10 +482,10 @@ void openbtnPressed(){ //open button pressed interrupt
   }
 }
 
-void openappPressed(){ //open signal from app interrupt         
-  if (state != OPEN){ // if already in open state do nothing, else stop motor
-  digitalWrite(IN1, MAX); 
-  digitalWrite(IN2, MAX);
-  state = OPEN;
-  }
-}
+// void RFIDbtnPressed(){ //open signal from app interrupt         
+//   if (state != OPEN){ // if already in open state do nothing, else stop motor
+//   digitalWrite(IN1, MAX); 
+//   digitalWrite(IN2, MAX);
+//   state = OPEN;
+//   }
+// }
